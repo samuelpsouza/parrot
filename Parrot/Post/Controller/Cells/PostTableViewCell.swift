0c2f932de28b855eff8004f4097a8590982166db
Samuel Souza <samuelsouza.contact@gmail.com>
2019-07-25 11:45:03 -0300

    adding post tableview

---
+++ Parrot/Post/Controller/Cells/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  parrot
+//
+//  Created by administrador on 25/07/19.
+//  Copyright © 2019 samuelpsouza. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
